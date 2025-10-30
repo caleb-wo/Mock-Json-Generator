0649eb38818f404f69621082af33a9b00bab9ab2
calebwolfe <caleb-wolfe@gmx.com>
2025-10-30 11:29:50 -0600

    Finished early implementation to get keys.

--- src/model/MockDataAssembler.ts
+++ src/model/MockDataAssembler.ts
@@ -1,3 +1,46 @@
+import * as rl from "node:readline/promises";
+import { stdin as input, stdout as output } from "node:process";
+
 export class MockDataAssembler {
-  test: string = "test";
+  static prepareReader(): rl.Interface {
+    return rl.createInterface({ input, output });
+  }
+
+  keys: Set<string> = new Set();
+  values: Map<string, any> = new Map();
+
+  async getKeys(set = this.keys): Promise<void> {
+    let reader = MockDataAssembler.prepareReader();
+
+    while (true) {
+      const rawKey = await reader.question(
+        "Enter a key (or 'done' to move on): ",
+      );
+      let key = rawKey.trim().toLowerCase();
+      if (key === "done") break;
+
+      if (set.has(key)) {
+        console.log(`Key '${key}' already exists.`);
+      } else {
+        console.log(`Key '${key}' added.`);
+        set.add(key);
+      }
+    }
+    console.log("Keys added.");
+    reader.close();
+  }
+}
+
+enum MockString {
+  word,
+  sentence,
+  paragraph,
+  essay,
+}
+
+enum MockNumber {
+  small,
+  medium,
+  large,
+  huge,
 }
